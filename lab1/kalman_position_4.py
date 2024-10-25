17a0b683367ee52a3b6ab698aba21f4d954131c5
Ege Turan <egeturan@stanford.edu>
2024-10-24 17:28:47 -0700

    error estimates

--- lab1/kalman_position_4.py
+++ lab1/kalman_position_4.py
@@ -55,7 +55,7 @@ dt = 0.01
 GEE_TO_METERS = 9.81  # 1 gee = 9.81 m/s²
 meas_var = GEE_TO_METERS**2*(0.01)**2 # from allan deviation average at sample size = 1
 
-raw_measurements_accel, raw_measurements_gyro = extract_values_from_file("/Users/egeturan/Documents/Sensing/SmartphoneSensors292S/ee292s/lab1/oct23/6ft_medium_round_straight_c")
+raw_measurements_accel, raw_measurements_gyro = extract_values_from_file("/Users/egeturan/Documents/Sensing/SmartphoneSensors292S/ee292s/lab1/oct23/6ft_medium_round_straight")
 raw_accel = 9.81*np.array(raw_measurements_accel)
 raw_gyro = np.array(raw_measurements_gyro)
 
@@ -73,14 +73,14 @@ kf.x = np.zeros(3)
         
 # Initial covariance matrix (high uncertainty)
 kf.P = np.eye(3)
-kf.P *= 1e-4
+kf.P *= 1e-2
         
 # Process noise covariance matrix (assuming very stable process)
 # kf.Q = np.array([[0., 0., 0.],
 #                  [0., 0., 0.],
 #                  [0., 0., 0.]])
 # kf.Q = Q_discrete_white_noise(dim=3, dt=0.1, var=0.1)
-kf.Q = np.eye(3)*1e1
+kf.Q = np.eye(3)*1e3
 
 meas_var = 1e2
 # Measurement noise (variance in the accel measurements)
@@ -92,14 +92,14 @@ taxis = np.array(range(num_samples))
 # Example loop for Kalman filter using accelerometer data
 for t in range(num_samples):
     z = raw_accel[t][0]  # Use the X-axis acceleration in world frame
-    z = 0 if (np.abs(z) < 0.1) else z
+    z = 0 if (np.abs(z) < 0.1) else z 
 
     # Predict step (use accelerometer data)
     kf.predict()
     kf.update(z)
 
     # Get the estimated state (position, velocity)
-    # kf.x[1] = kf.x[1]*0.95 if (np.abs(kf.x[0]) < 0.01) else kf.x[1]
+    # kf.x[1] = kf.x[1]*0.99 if (np.abs(kf.x[1]) < 0.1) else kf.x[1]
     estimated_position, estimated_velocity, estimated_accel = kf.x
     p_estimates.append(estimated_position)
     v_estimates.append(estimated_velocity)
@@ -114,29 +114,31 @@ for t in range(num_samples):
 fig1, axs1 = plt.subplots(3, 1, figsize=(8, 10))
 fig1.suptitle('Kalman Filter Estimates')
 
+taxis = taxis * dt
+
 # Position
 p_estimates = np.array(p_estimates)
-axs1[0].plot(taxis*dt, p_estimates, label='KF position estimate')
+axs1[0].plot(taxis, p_estimates, label='KF position estimate')
 axs1[0].set_ylabel('Position (m)')
 axs1[0].grid()
 axs1[0].legend()
 
 # Velocity
 v_estimates = np.array(v_estimates)
-axs1[1].plot(taxis*dt, v_estimates, label='KF velocity estimate')
+axs1[1].plot(taxis, v_estimates, label='KF velocity estimate')
 axs1[1].set_ylabel('Velocity (m/s)')
 axs1[1].grid()
 axs1[1].legend()
 
 # Accel
 a_estimates = np.array(a_estimates)
-axs1[2].plot(taxis*dt, a_estimates, label='KF accel estimate')
+axs1[2].plot(taxis, a_estimates, label='KF accel estimate')
 axs1[2].set_ylabel('Acceleration (m/s^2)')
 axs1[2].grid()
 axs1[2].legend()
 
 fig3 = plt.figure()
-plt.plot(taxis/10, raw_accel, label='raw accel x')
+plt.plot(taxis, raw_accel, label=['raw accel x','raw accel y','raw accel z'])
 
 
 plt.legend()
@@ -150,19 +152,51 @@ fig2.suptitle('Kalman Gains')
 kalman_gains_array = np.array(kalman_gains)
 
 # Gain for Position
-axs2[0].plot(taxis/10, kalman_gains_array[:, 0, :], label='Kalman Gain Position')
+axs2[0].plot(taxis, kalman_gains_array[:, 0, :], label='Kalman Gain Position')
 axs2[0].set_ylabel('Position Gain')
 axs2[0].grid()
 
 # Gain for Velocity
-axs2[1].plot(taxis/10, kalman_gains_array[:, 1, :], label='Kalman Gain Velocity')
+axs2[1].plot(taxis, kalman_gains_array[:, 1, :], label='Kalman Gain Velocity')
 axs2[1].set_ylabel('Velocity Gain')
 axs2[1].grid()
 
 # Gain for Accel
-axs2[2].plot(taxis/10, kalman_gains_array[:, 2, :], label='Kalman Gain Accel')
+axs2[2].plot(taxis, kalman_gains_array[:, 2, :], label='Kalman Gain Accel')
 axs2[2].set_ylabel('Accel Gain')
 axs2[2].grid()
+
+fig4 = plt.figure()
+# Define the parameters
+x_start = 2.1
+x_end = 8.1
+slope = 1.8 / 6
+x_start_b = 10.6
+x_end_b = 16.6
+
+y = np.zeros(len(taxis))
+# Calculate y values based on the line equation y = slope * (x - x_start)
+y[int(x_start/dt):int(x_end/dt)] = slope * (taxis[int(x_start/dt):int(x_end/dt)] - x_start)
+y[int(x_end/dt):int(x_start_b/dt)] = y[int(x_end/dt)-1]
+y[int(x_start_b/dt):int(x_end_b/dt)] = -slope * (taxis[int(x_start_b/dt):int(x_end_b/dt)] - x_start_b) + y[int(x_end/dt)-1]
+
+# Plot the line
+plt.plot(taxis, y, label=f'ideal constant velocity path')
+plt.plot(taxis, p_estimates, label=f'kalman position estimate')
+plt.plot(taxis, abs(y-p_estimates), label=f'error', c='red')
+
+
+# Add labels and title
+plt.xlabel('time (s)')
+plt.ylabel('position (m)')
+plt.title('Position estimate error as a function of time')
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.legend()
+
+# Show the plot
+plt.grid(True)
+plt.show()
 
 
 plt.tight_layout(rect=[0, 0, 1, 0.96])  # Adjust layout and leave space for the title
